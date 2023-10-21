6f99c057ac0bb68ef005cd43bd93ed4d8b87af00
Bruno Fernando <bruunofernandonunes@gmail.com>
2023-10-21 16:15:01 -0300

    fix order button

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,7 @@ import {
   getDownloadURL,
   listAll,
   deleteObject,
+  updateMetadata,
 } from "firebase/storage";
 import { storage } from "../../firebase";
 import ReactPlayer from "react-player";
@@ -21,59 +22,33 @@ export default function Home() {
   const [activeAudioIndex, setActiveAudioIndex] = useState(null);
   const [isAudioPlaying, setIsAudioPlaying] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [isEditing, setIsEditing] = useState(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [deleteItemIndex, setDeleteItemIndex] = useState(null);
 
-  async function listFiles() {
-    try {
-      const storageRef = ref(storage, "/images");
-      const files = await listAll(storageRef);
-
-      const fileArray = [];
-
-      for (const item of files.items) {
-        const url = await getDownloadURL(item);
-        fileArray.push({ name: item.name, downloadURL: url });
-      }
-
-      setFileList(fileArray);
-      setIsLoading(false);
-    } catch (error) {
-      console.error("Erro ao listar arquivos: " + error);
-      setIsLoading(false);
-    }
-  }
-
   useEffect(() => {
+    async function listFiles() {
+      try {
+        const storageRef = ref(storage, "/images");
+        const files = await listAll(storageRef);
+
+        const fileArray = [];
+
+        for (const item of files.items) {
+          const url = await getDownloadURL(item);
+          fileArray.push({ name: item.name, downloadURL: url });
+        }
+
+        setFileList(fileArray);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Erro ao listar arquivos: " + error);
+        setIsLoading(false);
+      }
+    }
+
     listFiles();
   }, []);
-
-  const handleDelete = async (index) => {
-    // Abra o modal de exclusão
-    openDeleteModal(index);
-  };
-
-  const openDeleteModal = (index) => {
-    setDeleteItemIndex(index);
-    setIsDeleteModalOpen(true);
-  };
-
-  const confirmDelete = () => {
-    const index = deleteItemIndex;
-    if (index !== null) {
-      const fileToDelete = fileList[index];
-      const storageRef = ref(storage, `/images/${fileToDelete.name}`);
-      deleteObject(storageRef);
-
-      const updatedFileList = [...fileList];
-      updatedFileList.splice(index, 1);
-      setFileList(updatedFileList);
-    }
-
-    setIsDeleteModalOpen(false);
-  };
-
   useEffect(() => {
     const handleKeyDown = (event) => {
       if (event.key >= "1" && event.key <= "9") {
@@ -96,6 +71,78 @@ export default function Home() {
     };
   }, [fileList, activeAudioIndex, isAudioPlaying]);
 
+  const handleDelete = async (index) => {
+    openDeleteModal(index);
+  };
+  const handleAudioPlayPause = (index) => {
+    if (activeAudioIndex === index) {
+      // Se o índice pressionado for igual ao índice ativo
+      setIsAudioPlaying((prevIsAudioPlaying) => !prevIsAudioPlaying); // Alternar reprodução/pausa
+    } else {
+      // Caso contrário, defina o novo índice e comece a reprodução
+      setActiveAudioIndex(index);
+      setIsAudioPlaying(true);
+    }
+  };
+  const openDeleteModal = (index) => {
+    setDeleteItemIndex(index);
+    setIsDeleteModalOpen(true);
+  };
+
+  const confirmDelete = async () => {
+    const index = deleteItemIndex;
+
+    if (index !== null) {
+      const fileToDelete = fileList[index];
+      const storageRef = ref(storage, `/images/${fileToDelete.name}`);
+      await deleteObject(storageRef);
+
+      const updatedFileList = [...fileList];
+      updatedFileList.splice(index, 1);
+      setFileList(updatedFileList);
+    }
+
+    setIsDeleteModalOpen(false);
+  };
+
+  const handleStartEditing = () => {
+    setIsEditing(true);
+  };
+
+  const handleSaveOrder = async () => {
+    const storageRef = ref(storage, "/images");
+    for (let i = 0; i < fileList.length; i++) {
+      const file = fileList[i];
+      console.log(file);
+      await updateMetadata(ref(storageRef, file.name), {
+        customMetadata: { order: i },
+      });
+    }
+    setIsEditing(false);
+  };
+
+  const handleCancelEdit = () => {
+    // Restaure a ordem original
+    setIsEditing(false);
+    setFileList([...fileList]);
+  };
+
+  const handleDragStart = (e, index) => {
+    e.dataTransfer.setData("text/plain", index);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e, index) => {
+    const draggedIndex = e.dataTransfer.getData("text/plain");
+    const updatedList = [...fileList];
+    const [draggedItem] = updatedList.splice(draggedIndex, 1);
+    updatedList.splice(index, 0, draggedItem);
+    setFileList(updatedList);
+  };
+
   return (
     <>
       <Header />
@@ -106,8 +153,15 @@ export default function Home() {
         ) : (
           <ul>
             {fileList.map((file, index) => (
-              <li key={index}>
+              <li
+                key={index}
+                onDragStart={(e) => handleDragStart(e, index)}
+                onDragOver={handleDragOver}
+                onDrop={(e) => handleDrop(e, index)}
+                draggable={isEditing}
+              >
                 <div>
+                  <span>Precione a tecla: {index + 1}</span>
                   <span>Nome do arquivo: {file.name}</span>
                   <ReactPlayer
                     url={file.downloadURL}
@@ -115,19 +169,46 @@ export default function Home() {
                     height={"20%"}
                     width={"80%"}
                     playing={index === activeAudioIndex && isAudioPlaying}
+                    onEnded={() => {
+                      if (index === activeAudioIndex && isAudioPlaying) {
+                        setActiveAudioIndex(null);
+                        setIsAudioPlaying(false);
+                        setTimeout(() => {
+                          setActiveAudioIndex(index);
+                          setIsAudioPlaying(true);
+                        }, 100);
+                      }
+                    }}
+                    loop 
                   />
-                  <div className="buttons-box">
-                    <button onClick={() => handleDelete(index)}>Excluir</button>
-                  </div>
+
+                  <button
+                    id="delete-button"
+                    onClick={() => handleDelete(index)}
+                  >
+                    Excluir
+                  </button>
                 </div>
               </li>
             ))}
           </ul>
         )}
 
+        {isEditing ? (
+          <div className="buttons-box">
+            <button id="save" onClick={handleSaveOrder}>
+              Salvar Ordem
+            </button>
+            <button onClick={handleCancelEdit}>Cancelar</button>
+          </div>
+        ) : (
+          <button className="edit-button" onClick={handleStartEditing}>
+            Editar Ordem
+          </button>
+        )}
+
         {isDeleteModalOpen && (
           <DeleteModal
-            
             onConfirm={confirmDelete}
             onClose={() => setIsDeleteModalOpen(false)}
           />
--- src/styles/home.js
+++ src/styles/home.js
@@ -1,68 +1,85 @@
 import { styled } from ".";
 
-export const HomeContainer = styled('main',{
+export const HomeContainer = styled("main", {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  maxWidth: "800px",
+  margin: "0 auto ",
+  fontFamily: "sans-serif",
+  ul: {
     display: "flex",
-    flexDirection:'column',
-    justifyContent:'center',
-    alignItems:'center',
-    maxWidth:'800px',
-    margin: '0 auto ',
-    fontFamily:'sans-serif',
-    ul:{
-        display:'flex',
-        flexWrap:'wrap',
-       width:'100%',
-       marginTop:'4rem',
-       gap:'1rem',
-    },
-    li:{
-        display: "flex",
-        flexDirection:'column',
-        justifyContent:'space-between',
-        alignItems:'center',
-        width:'215px',
-        height:'159px',
-        background:'#43C3DF',
-        padding:10,
-        borderRadius:'10px',
-        fontWeight:'bolder',
-        border:'4px solid #231F20'
+    flexWrap: "wrap",
+    width: "100%",
+    marginTop: "4rem",
+    gap: "1rem",
+  },
+  li: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "215px",
+    height: "159px",
+    background: "#43C3DF",
+    padding: 10,
+    borderRadius: "10px",
+    fontWeight: "bolder",
+    border: "4px solid #231F20",
+  },
+  "li div": {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  "li div div": {
+    width: "100%",
+    height: "100%",
+  },
+  audio: {
+    width: 200,
+  },
+  ".buttons-box": {
+    display: "flex",
+    height: "23%",
+    flexDirection: "row-reverse",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "39%",
+    marginTop: "3rem",
+  },
+  ".buttons-box button": {
+    padding: "9px",
+    borderRadius: "5px",
+    width: "40%",
+    background: "#231F20",
+    color: "$white",
+    fontWeight: "bolder",
+  },
+  h2: {
+    fontSize: "3rem",
+    color: "$white",
+    marginTop: "3rem",
+  },
+  ".edit-button": {
+    padding: "15px",
+    borderRadius: "5px",
 
-    },
-    'li div':{
-        width:'100%',
-        height:'100%',
-        display: "flex",
-        flexDirection:'column',
-        justifyContent:'space-between',
-        alignItems:'center',
-    },
-    'li div div':{
-        width:'100%',
-        height:'100%'
-    },
-    audio:{
-        width:200
-    },
-    '.buttons-box':{
-        display:'flex',
-        height:'23%',
-        flexDirection:'row-reverse',
-        alignItems:"center"
-    },
-    '.buttons-box button':{
-        padding:'6px',
-        borderRadius:'5px',
-        width:'40%',
-        background:'#231F20',
-        color:'$white',
-        fontWeight:'bolder',
-        padding:'5px'
-    },
-    h2:{
-        fontSize:'3rem',
-        color:'$white',
-        marginTop:'3rem'
-    }
+    background: 'rgb(230,179,142)',
+    background: 'radial-gradient(circle, rgba(230,179,142,1) 0%, rgba(218,138,81,1) 52%, rgba(218,138,81,1) 100%)',
+    color: "$white",
+    fontWeight: "bolder",
+  },
+  button: {
+    padding: "7px",
+    borderRadius: "5px",
 
-})
+    background: "#231F20",
+    color: "$white",
+    fontWeight: "bolder",
+  },
+});
